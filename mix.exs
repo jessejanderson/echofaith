18b2e6956dc74e8444155441377a5be9fd5db995
Jesse J. Anderson <jessejanderson@gmail.com>
2016-01-22 18:17:40 -0800

    add credo to mix.exs dependencies

--- mix.exs
+++ mix.exs
@@ -35,7 +35,9 @@ defmodule Echofaith.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.2", only: [:dev, :test]}
+    ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
