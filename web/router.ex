3c50e5672a0cda84bbd0aad5c80c33c1292ba8f3
Jesse J. Anderson <jessejanderson@gmail.com>
2016-01-22 17:42:47 -0800

    add resource '/posts', PostController to router.ex

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Echofaith.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/posts", PostController
   end
 
   # Other scopes may use custom stacks.
