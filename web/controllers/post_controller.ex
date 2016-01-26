f403ea2337db8880d82545866f8da77559ffe618
Jesse J. Anderson <jessejanderson@gmail.com>
2016-01-25 23:55:05 -0800

    update PostController with new user/post association

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -7,17 +7,21 @@ defmodule Echofaith.PostController do
   plug :assign_user
 
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Repo.all(assoc(conn.assigns[:user], :posts))
     render(conn, "index.html", posts: posts)
   end
 
   def new(conn, _params) do
-    changeset = Post.changeset(%Post{})
+    changeset = conn.assigns[:user]
+    |> build(:posts)
+    |> Post.changeset(%Post{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"post" => post_params}) do
-    changeset = Post.changeset(%Post{}, post_params)
+    changeset = conn.assigns[:user]
+    |> build(:posts)
+    |> Post.changeset(%Post{}, post_params)
 
     case Repo.insert(changeset) do
       {:ok, _post} ->
@@ -30,18 +34,21 @@ defmodule Echofaith.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    # post = conn.assigns[:user]
+    # |> assoc(:posts)
+    # |> Repo.get!(id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     render(conn, "show.html", post: post)
   end
 
   def edit(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     changeset = Post.changeset(post)
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     changeset = Post.changeset(post, post_params)
 
     case Repo.update(changeset) do
@@ -55,7 +62,7 @@ defmodule Echofaith.PostController do
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -66,8 +73,15 @@ defmodule Echofaith.PostController do
     |> redirect(to: user_post_path(conn, :index, conn.assigns[:user]))
   end
 
-  defp assign_user(conn, %{"user_id" => user_id}) do
-    user = Repo.get(Echofaith.User, user_id)
-    assign(conn, :user, user)
+  defp assign_user(conn, _) do
+    %{"user_id" => user_id} = conn.params
+    if user = Repo.get(Echofaith.User, user_id) do
+      assign(conn, :user, user)
+    else
+      conn
+      |> put_flash(:error, "Invalid user!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
